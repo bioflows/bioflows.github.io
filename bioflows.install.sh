ffaaff8b1ddbd6d409e77b029805d5474f1373e1
Mohamed Fawzy <mfawzy.sami@gmail.com>
2020-10-21 11:17:02 +0300

    Adding bioflows installation script

---
+++ bioflows.install.sh
@@ -0,0 +1,14 @@
+echo "Installing Bioflows..."
+echo "###########################Installing Docker###########################"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.#!/bin/sh
+echo "adding your current username to docker group"
+sudo usermod -aG docker $1
+echo "Starting Docker Engine..."
+sudo service docker start
+echo "###########################Installing Consul###########################"
+curl -O https://releases.hashicorp.com/consul/1.8.4/consul_1.8.4_linux_amd64.zip
+echo "Unzipping consul"
+unzip consul_1.8.4_linux_amd64.zip
+sudo mv consul /usr/bin
+sudo chmod a+x /usr/bin/consul
